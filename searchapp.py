9cf6a7e4f5a8c1199ac61dc60156384d9e1c2d19
hiwa4h <72159083+hiwa4h@users.noreply.github.com>
2021-01-12 18:02:36 +0300

    Create searchapp.py

---
+++ searchapp.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QTableView, QHeaderView, QVBoxLayout
+from PyQt5.QtCore import Qt, QSortFilterProxyModel
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+
+class AppDemo(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(800, 400)
+        mainLayout = QVBoxLayout()
+
+        companies = ('Hiwa', 'hojen', 'NAME0', 'NAME', 'FBI')
+        model = QStandardItemModel(len(companies), 1)
+        model.setHorizontalHeaderLabels(['Names'])
+
+        for row, company in enumerate(companies):
+            item = QStandardItem(company)
+            model.setItem(row, 0, item)
+
+        filter_proxy_model = QSortFilterProxyModel()
+        filter_proxy_model.setSourceModel(model)
+        filter_proxy_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        filter_proxy_model.setFilterKeyColumn(0)
+
+        search_field = QLineEdit()          
+        search_field.setStyleSheet('font-size: 35px; height: 60px;')
+        search_field.textChanged.connect(filter_proxy_model.setFilterRegExp)
+        mainLayout.addWidget(search_field)
+
+        table = QTableView()
+        table.setStyleSheet('font-size: 35px;')
+        table.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table.setModel(filter_proxy_model)
+        mainLayout.addWidget(table)
+
+        self.setLayout(mainLayout)
+
+app = QApplication(sys.argv)
+demo = AppDemo()
+demo.show()
+sys.exit(app.exec_())
